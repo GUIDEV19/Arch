14da8af8ba0552ee58c572bad504e102baa1dfca
Guilherme <guilherme@zscansoftware.com>
2023-02-06 21:30:04 -0300

    Responsividade mobile page contact.

--- contact.js
+++ contact.js
@@ -1,4 +1,6 @@
 const target = document.querySelectorAll("[data-anime]");
+const menuMobile = document.querySelector("#menu-mobile");
+const options = document.querySelector("#menu-mobile-click");
 function animeScroll(element) {
     const windowTop = window.pageYOffset + ((window.innerHeight + 3)/ 4);
     element.forEach((e) => {
@@ -12,4 +14,12 @@ function animeScroll(element) {
 
 window.addEventListener("scroll", () => {
     animeScroll(target);
+});
+
+menuMobile.addEventListener("click", () => {
+    if (options.classList.contains("optionsMenu")) {
+        options.classList.remove("optionsMenu")
+    } else {
+        options.classList.add("optionsMenu")
+    }
 });
